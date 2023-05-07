405f1534c3f06c1545434a782580312b2fdb99f8
shannu boi <shayan.khan.xyz@gmail.com>
2023-05-08 00:31:49 +0500

    Fixed line-break bug in article pages

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -6,8 +6,13 @@
     @else
       @foreach($articles as $article) 
       <x-small-card class="my-2">
-        <h4 class="text-xl font-semibold"><a href={{"/articles/".$article->id}}>{{$article->title}}</a></h4>
-        <p class="text-base transform -translate-x-1/2">{{nl2br(substr($article->body, 0, 256)).'...'}}</p>
+        <h3 class="text-xl font-semibold py-1"><a href={{"/articles/".$article->id}}>{{$article->title}}</a></h4>
+        <p class="text-base transform -translate-x-1/2">
+          @php
+          $mystr = substr($article->body, 0, 256).'...';
+          echo nl2br($mystr);
+          @endphp
+        </p>
         <p class="text-xs text-right">By {{$article->author}} &emsp; Date: {{$article->published_on}}</p>
       </x-small-card>
       @endforeach
