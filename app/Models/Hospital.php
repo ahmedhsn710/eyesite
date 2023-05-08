ef95cded7fa35a10d903ef7f9cf6aa29518a8575
shannu boi <shayan.khan.xyz@gmail.com>
2023-05-08 23:29:42 +0500

    Added city filtering

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Hospital extends Model
 {
     use HasFactory;
+
+    public function scopeFilter($query, $filter) {
+        if($filter['city'] ?? false) {
+            $query->where('city', '=', $filter['city']);
+        }
+    }
 }
