5924d2aa747d8f146bbc3879a8cc8e2524c6d207
shannu boi <shayan.khan.xyz@gmail.com>
2023-05-08 23:28:59 +0500

    Fixed minor bugs in hospital view

--- resources/views/hospitals/index.blade.php
+++ resources/views/hospitals/index.blade.php
@@ -1,30 +1,30 @@
 <x-app-layout>
   <x-main-card>
     <x-page-heading>Eye Hospitals</x-page-heading><br>
-    <div style="display: grid; grid-template-columns: auto auto;">
     @if(count($hospitals) == 0)
       <p>No hospitals found</p>
     @else
+    <div style="display: grid; grid-template-columns: 50% 50%;">
       @foreach($hospitals as $hospital)
         <x-small-card class="mx-6 my-3">
 
           <h3 class="text-xl font-semibold">{{$hospital->name}}</h3>
 
-          <p>City: <a href="/hospitals?city={{$hospital->city}}">{{$hospital->city}}</a></p>
+          <p>City: <u><a href="/hospitals?city={{$hospital->city}}">{{$hospital->city}}</a></u></p>
 
-          @if($hospital->address)
+          @if($hospital->address ?? false)
             <p class="text-xs">Full address: {{$hospital->address}}</p>
           @endif
 
-          @if($hospital->contact_no)
+          @if($hospital->contact_no ?? false)
           <p class="text-xs">Contact no: {{$hospital->contact_no}}</p>
           @endif
 
-          @if($hospital->email)
+          @if($hospital->email ?? false)
             <p class="text-xs">Email: {{$hospital->email}}</p>
           @endif
 
-          @if($hospital->website)
+          @if($hospital->website ?? false)
             <br><p class="text-sm text-right">
               Website: <a class="text-blue-500" href="{{$hospital->website}}">{{$hospital->website}}</a>
             </p>
@@ -32,7 +32,7 @@
 
         </x-small-card>
       @endforeach
-    @endif
-    </div>        
+    </div>       
+    @endif 
   </x-main-card>
 </x-app-layout>
